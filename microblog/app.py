9d18e82c2498cd53c96e809a409db2d4eafbc362
Israel Silva <israel.bernardes.silva@gmail.com>
2024-01-08 14:51:56 -0300

    criando rotas para user e apis (post, get_all e get_por_username) via  para user

--- microblog/app.py
+++ microblog/app.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 
+from .routes import main_router
+
 
 app = FastAPI(
     title="Microblog",
     version="0.1.0",
     description="Microblog is a posting app",
 )
+app.include_router(main_router)
 
 
 @app.get("/")
--- microblog/models/user.py
+++ microblog/models/user.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
 
+from pydantic import BaseModel
+
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -9,3 +11,17 @@ class User(SQLModel, table=True):
     avatar: Optional[str] = None
     bio: Optional[str] = None
     password: str = Field(nullable=False)
+
+
+class UserResponse(BaseModel):
+    username: str
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
+
+
+class UserRequest(BaseModel):
+    email: str
+    username: str
+    password: str
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
